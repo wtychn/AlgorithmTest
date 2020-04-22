63260cf4f381a5360ca4d514f1515b1a426d51ad
wtychn <452443579@qq.com>
2020-04-23 01:00:30 +0800

    Offer_13_ActivityScope method 1：DFS improved method 2:BFS passed

--- src/main/java/com/algorithm/search/Offer_13_ActivityScope.java
+++ src/main/java/com/algorithm/search/Offer_13_ActivityScope.java
@@ -1,33 +1,46 @@
 package com.algorithm.search;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Offer_13_ActivityScope {
-    public int movingCount(int m, int n, int k) {
-        boolean[][] table = new boolean[m][n];
-        return countSum(table, 0, 0, k, 0);
+    boolean[][] table;
+    int k;
+    public int movingCount_BFS(int m, int n, int k) {
+        table = new boolean[m][n];
+        this.k = k;
+        return countSum(0, 0, 0, 0);
     }
 
-    private int countSum(boolean[][] table, int row, int column, int k, int count) {
-        int sum = 0, r = row, c = column;
-        while (r / 10 > 0) {
-            sum += r % 10;
-            r = r / 10;
-        }
-        sum += r;
-        while (c / 10 > 0) {
-            sum += c % 10;
-            c = c / 10;
-        }
-        sum += c;
-        if (sum > k
+    private int countSum(int row, int column, int sr, int sc) {
+        if (sr + sc > k
                 || row > table.length - 1
                 || column > table[0].length - 1
                 || table[row][column]) {
-            return count;
+            return 0;
         }
-        count++;
         table[row][column] = true;
-        count = countSum(table, row + 1, column, k, count);
-        count = countSum(table, row, column + 1, k, count);
-        return count;
+        return 1 + countSum(row + 1, column, (row + 1) % 10 == 0 ? sr - 8 : sr + 1, sc)
+                + countSum(row, column + 1, sr, (column + 1) % 10 == 0 ? sc - 8 : sc + 1);
+    }
+    
+
+    public int movingCount_DFS(int m, int n, int k) {
+        boolean[][] visited = new boolean[m][n];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0, 0, 0});
+        int result = 0;
+        while (!queue.isEmpty()) {
+            int[] index = queue.poll();
+            int i = index[0], j = index[1], si = index[2], sj = index[3];
+            if (i > m - 1 || j > n - 1 || k < si + sj || visited[i][j]) {
+                continue;
+            }
+            result++;
+            visited[i][j] = true;
+            queue.add(new int[]{i + 1, j, (i + 1) % 10 == 0 ? si - 8 : si + 1, sj});
+            queue.add(new int[]{i, j + 1, si, (j + 1) % 10 == 0 ? sj - 8 : sj + 1});
+        }
+        return result;
     }
 }
--- src/test/java/com/algorithm/search/Offer_13_ActivityScopeTest.java
+++ src/test/java/com/algorithm/search/Offer_13_ActivityScopeTest.java
@@ -9,7 +9,9 @@ public class Offer_13_ActivityScopeTest {
     @Test
     public void movingCount() {
         Offer_13_ActivityScope activityScope = new Offer_13_ActivityScope();
-        int result = activityScope.movingCount(2, 3, 1);
-        assertEquals(3, result);
+        int result_BFS = activityScope.movingCount_BFS(38, 15, 9);
+        int result_DFS = activityScope.movingCount_DFS(2, 3, 1);
+        assertEquals(135, result_BFS);
+        assertEquals(3, result_DFS);
     }
 }
