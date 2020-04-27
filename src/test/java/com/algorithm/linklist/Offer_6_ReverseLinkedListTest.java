c2ad33a29c4f2266add549ba76fcfb41be7973ce
wty <richard_wty@163.com>
2020-04-27 10:55:17 +0800

    Offer_18_DeleteNode method 1:two pointers passed Add ListNode test and print method

--- src/test/java/com/algorithm/linklist/Offer_6_ReverseLinkedListTest.java
+++ src/test/java/com/algorithm/linklist/Offer_6_ReverseLinkedListTest.java
@@ -4,20 +4,11 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
-import static org.junit.Assert.*;
-
 public class Offer_6_ReverseLinkedListTest {
 
     @Test
     public void reversePrint() {
-        ListNode node1 = new ListNode(1);
-        ListNode node2 = new ListNode(3);
-        ListNode node3 = new ListNode(2);
-
-        node1.next = node2;
-        node2.next = node3;
-
         Offer_6_ReverseLinkedList reverseLinkedList = new Offer_6_ReverseLinkedList();
-        System.out.println(Arrays.toString(reverseLinkedList.reversePrint(node1)));;
+        System.out.println(Arrays.toString(reverseLinkedList.reversePrint(new ListNode().test())));
     }
 }
