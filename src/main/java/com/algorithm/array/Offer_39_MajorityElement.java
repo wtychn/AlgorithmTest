a7f2262cad29f55b9cebd9f1a805905516ae962e
wtychn <452443579@qq.com>
2020-07-14 13:31:08 +0800

    Offer_39_MajorityElement method 1: vote passed

--- src/main/java/com/algorithm/array/Offer_39_MajorityElement.java
+++ src/main/java/com/algorithm/array/Offer_39_MajorityElement.java
@@ -3,7 +3,7 @@ package com.algorithm.array;
 import java.util.HashMap;
 
 public class Offer_39_MajorityElement {
-    public int majorityElement(int[] nums) {
+    public int majorityElement_Hash(int[] nums) {
         HashMap<Integer, Integer> mapping = new HashMap<>();
         for (Integer num : nums) {
             if (mapping.containsKey(num)) {
@@ -17,5 +17,16 @@ public class Offer_39_MajorityElement {
             }
         }
         return -1;
+    }
+
+    public int majorityElement_Vote(int[] nums) {
+        int x = 0, votes = 0;
+        for (int num : nums) {
+            if (votes == 0) {
+                x = num;
+            }
+            votes += num == x ? 1 : -1;
+        }
+        return x;
     }
 }
