c017d1f2f177d8c213fb5315ffbf6fbe9fd251e5
wtychn <452443579@qq.com>
2020-07-21 13:14:29 +0800

    Offer_49_NthUglyNumberring method 1: dp passed

---
+++ src/test/java/com/algorithm/dynamic_programming/Offer_49_NthUglyNumberTest.java
@@ -0,0 +1,14 @@
+package com.algorithm.dynamic_programming;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Offer_49_NthUglyNumberTest {
+
+    @Test
+    public void nthUglyNumber() {
+        Offer_49_NthUglyNumber nthUglyNumber = new Offer_49_NthUglyNumber();
+        System.out.println(nthUglyNumber.nthUglyNumber(10));;
+    }
+}
