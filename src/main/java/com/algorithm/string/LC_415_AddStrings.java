8b402f3b90d9c30e17fb4899d6c46042f353e31b
wtychn <452443579@qq.com>
2020-08-13 11:49:17 +0800

    LC_415_AddStrings method 2: double point and arr passed

--- src/main/java/com/algorithm/string/LC_415_AddStrings.java
+++ src/main/java/com/algorithm/string/LC_415_AddStrings.java
@@ -32,4 +32,38 @@ public class LC_415_AddStrings {
         return result.reverse().toString();
     }
 
+    public String addStrings_Arr(String num1, String num2) {
+        int[] res = new int[Math.max(num1.length(), num2.length()) + 1];
+        int num1Index = num1.length() - 1, num2Index = num2.length() - 1, resIndex = res.length - 1;
+        while (num1Index >= 0 && num2Index >= 0) {
+            int sum = res[resIndex] + num1.charAt(num1Index) + num2.charAt(num2Index) - 2 * '0';
+            res[resIndex] = sum % 10;
+            res[resIndex - 1] += sum / 10;
+            num1Index--;
+            num2Index--;
+            resIndex--;
+        }
+        while (num1Index >= 0 || num2Index >= 0) {
+            int sum;
+            if (num1Index >= 0) {
+                sum = res[resIndex] + num1.charAt(num1Index) - '0';
+                res[resIndex] = sum % 10;
+                res[resIndex - 1] += sum / 10;
+                num1Index--;
+            } else {
+                sum = res[resIndex] + num2.charAt(num2Index) - '0';
+                res[resIndex] = sum % 10;
+                res[resIndex - 1] += sum / 10;
+                num2Index--;
+            }
+            resIndex--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < res.length; i++) {
+            if(i == 0 && res[i] == 0) continue;
+            sb.append(res[i]);
+        }
+        return sb.toString();
+    }
 }
