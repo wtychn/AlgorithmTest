937939dcf397888aeab38f7ec537a84a4eff15ee
wtychn <452443579@qq.com>
2020-10-12 15:04:08 +0800

    LC_530_GetMinimumDifference method 1: inorder passed

--- src/main/java/com/algorithm/App.java
+++ src/main/java/com/algorithm/App.java
@@ -2,23 +2,13 @@ package com.algorithm;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        List<Integer> integerList = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            if (i < 4 && i > 1) {
-                integerList.add(null);
-            } else {
-                integerList.add(i);
-            }
-        }
-        System.out.println(integerList.toString());
+public class App {
+    public static void main(String[] args) {
+
     }
 }
--- src/main/java/com/algorithm/array/LCWeek210_1_BracketDepth.java
+++ src/main/java/com/algorithm/array/LCWeek210_1_BracketDepth.java
@@ -4,6 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
+/***
+ * @Description: 求字符串括号深度
+ * @level easy
+ * @author wtychn
+ * @Date 2020/10/11
+ */
 public class LCWeek210_1_BracketDepth {
     public int maxDepth(String s) {
         if(s == null || s.equals("")) return 0;
