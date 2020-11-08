9dcc096b642233ee80cdc3a3378bf368ab50849a
wtychn <452443579@qq.com>
2020-11-08 16:23:39 +0800

    LC_121_MaxProfit method 1: double pointer passed

---
+++ src/test/java/com/algorithm/array/LC_121_MaxProfitTest.java
@@ -0,0 +1,15 @@
+package com.algorithm.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LC_121_MaxProfitTest {
+
+    @Test
+    public void maxProfit() {
+        LC_121_MaxProfit m = new LC_121_MaxProfit();
+        System.out.println(m.maxProfit(new int[]{1, 2, 3, 4, 5}));
+    }
+
+}
