bf7f77b61db4961331485d6397a4b8d0a1a73445
wtychn <452443579@qq.com>
2020-11-15 15:52:18 +0800

    LC_5601_OrderedStream method 1: array passed LC_5602_CloseStrings method 1: array&set passed LC_5603_MinOperations method 1: slidingWindow passed

---
+++ src/test/java/com/algorithm/string/LC_5602_CloseStringsTest.java
@@ -0,0 +1,14 @@
+package com.algorithm.string;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LC_5602_CloseStringsTest {
+
+    @Test
+    public void closeStrings() {
+        LC_5602_CloseStrings c = new LC_5602_CloseStrings();
+        System.out.println(c.closeStrings("abacb", "acbbb"));
+    }
+}
