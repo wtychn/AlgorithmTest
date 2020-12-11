b13bb84a5e114e1045b47166adfebed5d06337bb
wtychn <452443579@qq.com>
2020-12-12 00:43:24 +0800

    LC_649_PredictPartyVictory method 1: greedy method 2: two queue

---
+++ src/test/java/com/algorithm/array/LC_649_PredictPartyVictoryTest.java
@@ -0,0 +1,11 @@
+package com.algorithm.array;
+
+import junit.framework.TestCase;
+
+public class LC_649_PredictPartyVictoryTest extends TestCase {
+
+    public void testPredictPartyVictory() {
+        LC_649_PredictPartyVictory p = new LC_649_PredictPartyVictory();
+        System.out.println(p.predictPartyVictory("DRRD"));
+    }
+}
