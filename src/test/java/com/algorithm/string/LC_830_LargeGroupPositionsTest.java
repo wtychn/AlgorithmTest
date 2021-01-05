ce8694adc6c68586c764dc3dfe3258d9ef36a2e7
wtychn <452443579@qq.com>
2021-01-05 09:50:05 +0800

    LC_830_LargeGroupPositions method 1: double pointers passed

---
+++ src/test/java/com/algorithm/string/LC_830_LargeGroupPositionsTest.java
@@ -0,0 +1,14 @@
+package com.algorithm.string;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LC_830_LargeGroupPositionsTest {
+
+    @Test
+    public void largeGroupPositions() {
+        LC_830_LargeGroupPositions l = new LC_830_LargeGroupPositions();
+        System.out.println(l.largeGroupPositions("abbxxxxzzy"));
+    }
+}
