badde359e683b7bbc71d366977b69f64051fbbc4
wtychn <452443579@qq.com>
2021-01-24 00:25:41 +0800

    LC_5645_LargestAltitude method 1: travers passed

---
+++ src/main/java/com/algorithm/array/LC_5645_LargestAltitude.java
@@ -0,0 +1,17 @@
+package com.algorithm.array;
+/***
+ * @Description: 找到最高海拔  
+ * @level easy 
+ * @author wtychn
+ * @Date 2021/1/24
+ */
+public class LC_5645_LargestAltitude {
+    public int largestAltitude(int[] gain) {
+        int last = 0, max = 0;
+        for (int n : gain) {
+            max = Math.max(max, last + n);
+            last += n;
+        }
+        return max;
+    }
+}
