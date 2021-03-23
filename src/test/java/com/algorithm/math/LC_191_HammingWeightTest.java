a218967443dc7c9c2ff1cb41035803b8eb1370a7
wtychn <452443579@qq.com>
2021-03-23 09:01:51 +0800

    LC_191_HammingWeight method 1: bit passed

---
+++ src/test/java/com/algorithm/math/LC_191_HammingWeightTest.java
@@ -0,0 +1,14 @@
+package com.algorithm.math;
+
+import com.algorithm.bit.LC_191_HammingWeight;
+import org.junit.Test;
+
+public class LC_191_HammingWeightTest {
+
+    @Test
+    public void hammingWeight() {
+        int n = -3;
+        LC_191_HammingWeight h = new LC_191_HammingWeight();
+        System.out.println(h.hammingWeight(n));
+    }
+}
