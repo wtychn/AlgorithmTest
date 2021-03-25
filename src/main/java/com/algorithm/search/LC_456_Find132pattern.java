a28f53aa749d9095c91ebace1a6a26af97a98396
wtychn <452443579@qq.com>
2021-03-25 08:37:14 +0800

    LC_456_Find132pattern method 1: monotonic stack passed

---
+++ src/main/java/com/algorithm/search/LC_456_Find132pattern.java
@@ -0,0 +1,27 @@
+package com.algorithm.search;
+
+import java.util.Stack;
+/***
+ * @Description: 132模式  
+ * @level mid 
+ * @author wtychn
+ * @Date 2021/3/25
+ */
+public class LC_456_Find132pattern {
+    public boolean find132pattern(int[] nums) {
+        int[] min = new int[nums.length];
+        Stack<Integer> stack = new Stack<>();
+        min[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            min[i] = Math.min(min[i - 1], nums[i]);
+        }
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] > min[i]) {
+                while(!stack.isEmpty() && stack.peek() <= min[i]) stack.pop();
+                if(!stack.isEmpty() && stack.peek() < nums[i]) return true;
+                stack.push(nums[i]);
+            }
+        }
+        return false;
+    }
+}
---
+++ src/test/java/com/algorithm/search/LC_456_Find132patternTest.java
@@ -0,0 +1,14 @@
+package com.algorithm.search;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LC_456_Find132patternTest {
+
+    @Test
+    public void find132pattern() {
+        LC_456_Find132pattern f = new LC_456_Find132pattern();
+        System.out.println(f.find132pattern(new int[]{-1, 3, 2, 0}));
+    }
+}
