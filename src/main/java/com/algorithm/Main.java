d587eb8be46953a5fe29f31504f7f205acad0a8c
wtychn <452443579@qq.com>
2021-04-21 10:36:11 +0800

    LC_91_NumDecodings method 1: dp passed

--- src/main/java/com/algorithm/Main.java
+++ src/main/java/com/algorithm/Main.java
@@ -3,8 +3,30 @@ package com.algorithm;
 import java.util.*;
 
 public class Main {
-    public void solution() {
+    int[] nums;
+    List<List<Integer>> res = new ArrayList<>();
 
+    public List<List<Integer>> solution(int[] nums) {
+        this.nums = nums;
+        boolean[] isVisited = new boolean[nums.length];
+        Arrays.fill(isVisited, false);
+        dfs(new ArrayList<Integer>(), isVisited);
+        return res;
+    }
+
+    private void dfs(List<Integer> cur, boolean[] isVisited) {
+        if (cur.size() == nums.length) {
+            res.add(new ArrayList<>(cur));
+        }
+        for (int j = 0; j < nums.length; j++) {
+            if (!isVisited[j]) {
+                cur.add(nums[j]);
+                isVisited[j] = true;
+                dfs(cur, isVisited);
+                cur.remove(cur.size() - 1);
+                isVisited[j] = false;
+            }
+        }
     }
 
     public static void main(String[] args) {
