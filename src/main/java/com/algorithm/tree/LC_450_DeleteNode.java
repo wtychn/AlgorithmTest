0e85a9400742e927803a169cb73d347122ca5635
wtychn <452443579@qq.com>
2021-05-04 12:35:07 +0800

    LC_450_DeleteNode method 1: recur passed

---
+++ src/main/java/com/algorithm/tree/LC_450_DeleteNode.java
@@ -0,0 +1,44 @@
+package com.algorithm.tree;
+/***
+ * @Description: 删除二叉搜索树中的节点
+ * @level mid
+ * @author wtychn
+ * @Date 2021/5/4
+ */
+public class LC_450_DeleteNode {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) return null;
+        if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            if (root.left == null && root.right == null) {
+                root = null;
+            } else if (root.left == null) {
+                root.val = successor(root);
+                root.right = deleteNode(root.right, root.val);
+            } else {
+                root.val = predecessor(root);
+                root.left = deleteNode(root.left, root.val);
+            }
+        }
+        return root;
+    }
+
+    private int successor(TreeNode root) {
+        root = root.right;
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root.val;
+    }
+
+    private int predecessor(TreeNode root) {
+        root = root.left;
+        while (root.right != null) {
+            root = root.right;
+        }
+        return root.val;
+    }
+}
