c944b8b1afb745851130f00a6ba55f4e51c73caa
wtychn <452443579@qq.com>
2021-06-19 16:37:34 +0800

    LCS_2_HalfQuestions method 1: hashmap & sort passed

---
+++ src/test/java/com/algorithm/search/LCS12_LeastMinutesTest.java
@@ -0,0 +1,13 @@
+package com.algorithm.search;
+
+import com.algorithm.math.LCS_1_LeastMinutes;
+import org.junit.Test;
+
+public class LCS12_LeastMinutesTest {
+
+    @Test
+    public void leastMinutes() {
+        LCS_1_LeastMinutes l = new LCS_1_LeastMinutes();
+        System.out.println(l.leastMinutes(2));
+    }
+}
