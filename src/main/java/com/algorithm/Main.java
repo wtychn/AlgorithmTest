63e83878f0b585ef88a0b00ab01e1e035c87882b
wtychn <452443579@qq.com>
2021-08-01 15:40:48 +0800

    LC_1337_kWeakestRows method 1: priority queue passed method 2: traverse passed

--- src/main/java/com/algorithm/Main.java
+++ src/main/java/com/algorithm/Main.java
@@ -2,9 +2,12 @@ package com.algorithm;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
 
+    }
 }
 
 
