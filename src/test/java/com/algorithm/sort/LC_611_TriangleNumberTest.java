838b2eeb811159e7620268ae423492ff02458d9f
wtychn <452443579@qq.com>
2021-08-04 20:11:25 +0800

    LC_611_TriangleNumber method 1: sort & double pointers passed

---
+++ src/test/java/com/algorithm/sort/LC_611_TriangleNumberTest.java
@@ -0,0 +1,14 @@
+package com.algorithm.sort;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LC_611_TriangleNumberTest {
+
+    @Test
+    public void triangleNumber() {
+        LC_611_TriangleNumber t = new LC_611_TriangleNumber();
+        System.out.println(t.triangleNumber(new int[]{2, 2, 3, 4}));
+    }
+}
