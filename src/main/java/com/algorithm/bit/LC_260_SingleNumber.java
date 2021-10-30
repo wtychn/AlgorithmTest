320ac64e3e4dcd3708bd97b2777b2dff6e018719
wtychn <452443579@qq.com>
2021-10-30 20:30:59 +0800

    LC_260_SingleNumber method 1: bit passed

---
+++ src/main/java/com/algorithm/bit/LC_260_SingleNumber.java
@@ -0,0 +1,27 @@
+package com.algorithm.bit;
+
+/***
+ * @Description: 只出现一次的数字 3
+ * @level mid
+ * @author wtychn
+ * @Date 2021/10/30
+ */
+public class LC_260_SingleNumber {
+    public int[] singleNumber(int[] nums) {
+        int xorsum = 0;
+        for (int num : nums) {
+            xorsum ^= num;
+        }
+        // 防止溢出
+        int lsb = (xorsum == Integer.MIN_VALUE ? xorsum : xorsum & (-xorsum));
+        int type1 = 0, type2 = 0;
+        for (int num : nums) {
+            if ((num & lsb) != 0) {
+                type1 ^= num;
+            } else {
+                type2 ^= num;
+            }
+        }
+        return new int[]{type1, type2};
+    }
+}
